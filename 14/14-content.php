6e9ac2d623399417ec0b6af61724acf07fb4b544
nguyenthian49 <nguyenthiancdcntt10@gmail.com>
2019-10-31 23:54:31 +0700

    commit

--- 14/14-content.php
+++ 14/14-content.php
@@ -1,9 +1,10 @@
 <div class="type-14">
+    <div class="container">
     <!-- tieude -->
     <div class="tieude">
-        <div class="container">
+        
             <h1 class="fullwidth">FULLWIDTH</h1>
-        </div>
+        
     </div>
     <!-- hinh -->
     <div class="img">
@@ -12,7 +13,7 @@
             <!-- cot1 -->
             <div class="col-md-4">
                 <div class="container1">
-                    <img src="../14/images/img1.1.jpg" alt="">
+                    <img src="../14/images/img1.jpg" alt="">
                     <div class="overlay">
                         <div class="note1">
                             <i class="fa fa-heart-o tzicon"></i>
@@ -103,7 +104,7 @@
                     <div class="overlay">
                         <div class="note1">
                             <i class="fa fa-heart-o tzicon"></i>
-                            <h3><a href="#">Efficitur &#038; Lacus</a></h3>
+                            <h3><a href="#">Efficitur &#038;Lacus</a></h3>
                             <div class="tzmeta">
                                 <span>February 9, 2015</span>
                                 <span class="tztag">
@@ -136,138 +137,8 @@
                     </div>
                 </div>
                 </div>
-            </div>
-       
+            </div>  
         <!-- close dong2 -->
-<!--         dong3 
-        <div class="row">
-             cot1 
-            <div class="col-md-4">
-                <div class="container1">
-                    <img src="../14/images/img8.jpg" alt="">
-                    <div class="overlay">
-                        <div class="note1">
-                            <i class="fa fa-heart-o tzicon"></i>
-                            <h3><a href="#">Praesent Vitae</a></h3>
-                            <div class="tzmeta">
-                                <span>February 9, 2015</span>
-                                <span class="tztag">
-                                    /
-                                    <a href="#" rel="tag">NewYork</a>,
-                                    <a href="#" rel="tag">USA</a>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-             cot2 
-            <div class="col-md-4">
-                 <div class="container1">
-                    <img src="../14/images/img9.jpg" alt="">
-                    <div class="overlay">
-                        <div class="note1">
-                            <i class="fa fa-heart-o tzicon"></i>
-                            <h3><a href="#">Pretium Ucies Sapien</a></h3>
-                            <div class="tzmeta">
-                                <span>February 9, 2015</span>
-                                <span class="tztag">
-                                    /
-                                    <a href="#" rel="tag">NewYork</a>,
-                                    <a href="#" rel="tag">USA</a>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-             cot3 
-            <div class="col-md-4">
-                <div class="container1">
-                    <img src="../14/images/img10.jpg" alt="">
-                    <div class="overlay">
-                        <div class="note1">
-                            <i class="fa fa-heart-o tzicon"></i>
-                            <h3><a href="#">Varius Ut Tincidunt</a></h3>
-                            <div class="tzmeta">
-                                <span>February 9, 2015</span>
-                                <span class="tztag">
-                                    /
-                                    <a href="#" rel="tag">NewYork</a>,
-                                    <a href="#" rel="tag">USA</a>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-         close dong3 
-         dong4 
-        <div class="row">
-             cot1 
-            <div class="col-md-4">
-               <div class="container1">
-                    <img src="../14/images/img11.jpg" alt="">
-                    <div class="overlay">
-                        <div class="note1">
-                            <i class="fa fa-heart-o tzicon"></i>
-                            <h3><a href="#">Aliquam Sodalea</a></h3>
-                            <div class="tzmeta">
-                                <span>February 9, 2015</span>
-                                <span class="tztag">
-                                    /
-                                    <a href="#" rel="tag">NewYork</a>,
-                                    <a href="#" rel="tag">USA</a>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-             cot2 
-            <div class="col-md-4">
-               <div class="container1">
-                    <img src="../14/images/img12.jpg" alt="">
-                    <div class="overlay">
-                        <div class="note1">
-                            <i class="fa fa-heart-o tzicon"></i>
-                            <h3><a href="#">Fertum ac Lorem </a></h3>
-                            <div class="tzmeta">
-                                <span>February 9, 2015</span>
-                                <span class="tztag">
-                                    /
-                                    <a href="#" rel="tag">NewYork</a>,
-                                    <a href="#" rel="tag">USA</a>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-             cot3 
-            <div class="col-md-4">
-              <div class="container1">
-                    <img src="../14/images/img13.jpg" alt="">
-                    <div class="overlay">
-                        <div class="note1">
-                            <i class="fa fa-heart-o tzicon"></i>
-                            <h3><a href="#">Sed Congue Magna</a></h3>
-                            <div class="tzmeta">
-                                <span>February 9, 2015</span>
-                                <span class="tztag">
-                                    /
-                                    <a href="#" rel="tag">NewYork</a>,
-                                    <a href="#" rel="tag">USA</a>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
-        </div>-->
-        <!-- close dong4 -->
+    </div>
     </div>
