c5f6df5a4d2661aaf668c787d0f2dad6549b014b
ChucThang <tructhang2408@gmail.com>
2019-11-03 00:12:57 +0700

    add

---
+++ 22/22.php
@@ -0,0 +1,47 @@
+﻿<?php
+
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+$less->compileFile('less/22.less', 'css/22.css');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2</title>
+    <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link href="https://fonts.googleapis.com/css?family=Rouge+Script&display=swap" rel="stylesheet">
+    <link href="<?php echo $url_path ?>/css/fontawesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url_path ?>/css/22.css" rel="stylesheet" type="text/css" />
+    <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+
+    <link rel="stylesheet" href="<?php echo $url_path ?>/swiper-5.0.2/package/css/swiper.min.css">
+    <script src="<?php echo $url_path ?>/swiper-5.0.2/package/js/swiper.min.js"></script>
+
+</head>
+
+<body>
+    <?php include './22-content.php'; ?>
+    <script src="js/22.js"></script>
+
+</body>
+
+</html>
---
+++ 28/28-content.php
@@ -0,0 +1,89 @@
+<div class="type-28">
+    <div class="tzdefault_title">
+        <div class="container">
+            <h1>Guestbook</h1>
+
+
+            <div id="module-28" class="about-page">
+                <h3>Log in to post an entry</h3>
+                <form name="loginform" id="loginform">
+
+                    <p class="login-username">
+                        <label for="user_login">Username or Email Address</label>
+                        <input type="text" name="log" id="user_login" class="input" value="" size="20">
+                    </p>
+                    <p class="login-password">
+                        <label for="user_pass">Password</label>
+                        <input type="password" name="pwd" id="user_pass" class="input" value="" size="20">
+                    </p>
+
+                    <p class="login-remember"><label><input name="rememberme" type="checkbox" id="rememberme" value="forever"> Remember Me</label></p>
+                    <p class="login-submit">
+                        <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary" value="Log In">
+                        <input type="hidden" name="redirect_to" value="http://wordpress.templaza.net/everline/guestbook/">
+                    </p>
+
+                </form>
+            </div>
+            <div class="page-navigation"><span class="page-numbers current">1</span>
+                <a href="" title="Page 2">2</a>
+                <a href="" title="Page 3">3</a>
+                <a href="" title="Next page" rel="next">»</a>
+            </div>
+            <div class="box_comment">
+                <div id="gwolle-gb-total">13 entries.</div>
+                <div class="gb-entry ">
+                    <div class="gb-author-info">
+                        <span class="gb-author-avatar"><img alt="Jolie" src="./images/01.jpeg" class="avatar" height="32" width="32"></span>
+                        <span class="gb-author-name">Jolie
+                        </span>
+
+                        <span class="gb-datetime"><span class="gb-author-origin"> from Hanoi</span>
+                            <span class="gb-date"> wrote on October 15, 2015</span><span class="gb-time"> at 10:16 am</span>:
+                        </span>
+                    </div>
+                    <div class="gb-entry-content">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H.
+                    </div>
+                </div>
+                <div class="gb-entry ">
+                    <div class="gb-author-info">
+                        <span class="gb-author-avatar"><img alt="Jolie" src="./images/02.jpeg" class="avatar" height="32" width="32"></span>
+                        <span class="gb-author-name">Jolie
+                        </span>
+
+                        <span class="gb-datetime"><span class="gb-author-origin"> from Hanoi</span>
+                            <span class="gb-date"> wrote on October 15, 2015</span><span class="gb-time"> at 10:16 am</span>:
+                        </span>
+                    </div>
+                    <div class="gb-entry-content">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H.
+                    </div>
+                </div>
+                <div class="gb-entry ">
+                    <div class="gb-author-info">
+                        <span class="gb-author-avatar"><img alt="Jolie" src="./images/01.jpeg" class="avatar" height="32" width="32"></span>
+                        <span class="gb-author-name">Jolie
+                        </span>
+
+                        <span class="gb-datetime"><span class="gb-author-origin"> from Hanoi</span>
+                            <span class="gb-date"> wrote on October 15, 2015</span><span class="gb-time"> at 10:16 am</span>:
+                        </span>
+                    </div>
+                    <div class="gb-entry-content">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H.
+                    </div>
+                </div>
+                <div class="gb-entry ">
+                    <div class="gb-author-info">
+                        <span class="gb-author-avatar"><img alt="Jolie" src="./images/02.jpeg" class="avatar" height="32" width="32"></span>
+                        <span class="gb-author-name">Jolie
+                        </span>
+
+                        <span class="gb-datetime"><span class="gb-author-origin"> from Hanoi</span>
+                            <span class="gb-date"> wrote on October 15, 2015</span><span class="gb-time"> at 10:16 am</span>:
+                        </span>
+                    </div>
+                    <div class="gb-entry-content">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
