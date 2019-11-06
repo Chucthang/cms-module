0a7e2746df60eab0f7363c122582d09b93ced79b
Le Viet Hoang <33858683+hoangle02@users.noreply.github.com>
2019-11-06 07:45:15 +0700

    commit update

--- 8/js/8.js
+++ 8/js/8.js
@@ -17,25 +17,36 @@ $(document).ready(function() {
                 if (scrollTop < yBottomMedia && scrollBottom > yTopMedia) {
                     $(this).get(0).play();
                     interface.text('PAUSE');
+                   
                 } else {
                     $(this).get(0).pause();
                     interface.text('PLAY');
+
                 }
             }).on('ended', function() {
                 interface.text('PLAY');
             });
         }
-
+        
         interface.click(function() {
+            if($(".type-8 .bg-video").hasClass("none-bg")){
+                $(".type-8 .bg-video").removeClass("none-bg");
+            } else{
+                $(".type-8 .bg-video").addClass("none-bg");
+            }
+            if($('.bg-video').hasClass('none-bg')){
+                $('.autoplay').css('visibility', 'hidden');
+                $('.pause').css('visibility', 'visible');
 
-            $(this).text(function(i, v) {
-                return v === 'PLAY' ? 'PAUSE' : 'PLAY';
-            });
+            }else{
+                $('.autoplay').css('visibility', 'visible');
+                $('.pause').css('visibility', 'hidden');
+            }
 
             if (media[0].playing) media.get(0).pause();
             else media.get(0).play();
-        });
 
+        });
         $(window).scroll(checkMedia);
 
         Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
