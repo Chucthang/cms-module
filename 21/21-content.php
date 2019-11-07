c911094a4265902e306fb95cd7d11c7a887fd766
Nguyễn Thanh Phong <ntp4994@gmail.com>
2019-11-07 14:03:09 +0700

    commit

--- 21/21-content.php
+++ 21/21-content.php
@@ -1,4 +1,13 @@
-<div class="type-20">
+<?php
+    $url_host = 'http://' . $_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-21">
     <div class="container">
         <div class="test">
             <div class="events">
--- 28/28-content.php
+++ 28/28-content.php
@@ -1,4 +1,13 @@
-<div class="type-20">
+<?php
+    $url_host = 'http://' . $_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-28">
     <div class="container">
         <div class="test">
             <div class="events">
@@ -11,7 +20,6 @@
             <br>
             <br>
             <br>
-
             <div class="grid">
                 <h1>H1 Headline</h1>   
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse viverra mauris eget tortor imperdiet vehicula. Proin egestas diam ac mauris molestie hendrerit. Quisque nec nisi tortor. Etiam at mauris sit amet magna suscipit hend merit non sed ligula. Vivamus purus odio, mollis ut sagittis.<br>
