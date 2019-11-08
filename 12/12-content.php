030c1e069dde59afe3279268ea98e141fbef88dd
youchjckken <hieuphan2207@gmail.com>
2019-11-08 19:26:37 +0700

    commit

--- 12/12-content.php
+++ 12/12-content.php
@@ -64,15 +64,30 @@ $url_path = str_replace('\\', '/', $url_path);
                 Destination wedding photography – bring a photographer or use a local one? Wedding photography is the photography of activities relating to weddings. It encompasses photographs of the couple before marriage </p>
             <a class="more-link" href="#">READ MORE</a>
         </div>
-        <div class="video">
-            <div class="abg">
-                <video width="100%" controls>
-                    <source width="100%" src="./images/video.mp4" type="video/mp4">
-                </video>
+        <div class="type-video">
+            <!-- <div class="container"> -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="custom">
+                        <div class="wrapper">
+                            <div class="tz-video">
+                                <div class="tz-video-content">
+                                    <button class="autoplay"><i class="fa fa-video-camera"></i></button>
+                                    <button class="pause"><i class="fa fa-pause"></i></button>
+                                    <h3>“The Power of Love”</h3>
+                                    <p>JENNIFER AND JAMES WEDDING VIDEO</p>
+                                </div>
+                                <div class="bg-video"></div>
+                                <video class="videoID" style="margin-top: -379.406px;">
+                                    <source type="video/mp4" src="./images/video.mp4">                                  
+                            </video>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <button class="tzautoplay" style="display: inline-block;"><i class="fa fa-video-camera"></i></button>
+            <!-- </div> -->
         </div>
-
 
 
         <h2 class="title-post">
@@ -151,7 +166,7 @@ $url_path = str_replace('\\', '/', $url_path);
                 Destination wedding photography – bring a photographer or use a local one? Wedding photography is the photography of activities relating to weddings. It encompasses photographs of the couple before marriage </p>
             <a class="more-link" href="#">READ MORE</a>
         </div>
-        
+
     </div>
 
 
@@ -170,5 +185,62 @@ $url_path = str_replace('\\', '/', $url_path);
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+    });
+    $(document).ready(function() {
+        var media = $('video').not("[autoplay='autoplay']"),
+            interface = $('button'),
+            tolerance = 200;
+
+        function checkMedia() {
+
+            var current = $(window).scrollTop(),
+                scrollTop = current + tolerance,
+                scrollBottom = current + $(window).height() - tolerance;
+
+            media.each(function(index, el) {
+
+                var yTopMedia = $(this).offset().top,
+                    yBottomMedia = $(this).height() + yTopMedia;
+
+                if (scrollTop < yBottomMedia && scrollBottom > yTopMedia) {
+                    $(this).get(0).play();
+                    interface.text('PAUSE');
+
+                } else {
+                    $(this).get(0).pause();
+                    interface.text('PLAY');
+
+                }
+            }).on('ended', function() {
+                interface.text('PLAY');
+            });
+        }
+
+        interface.click(function() {
+            if ($(".type-video .bg-video").hasClass("none-bg")) {
+                $(".type-video .bg-video").removeClass("none-bg");
+            } else {
+                $(".type-video .bg-video").addClass("none-bg");
+            }
+            if ($('.bg-video').hasClass('none-bg')) {
+                $('.autoplay').css('visibility', 'hidden');
+                $('.pause').css('visibility', 'visible');
+
+            } else {
+                $('.autoplay').css('visibility', 'visible');
+                $('.pause').css('visibility', 'hidden');
+            }
+
+            if (media[0].playing) media.get(0).pause();
+            else media.get(0).play();
+
+        });
+        $(window).scroll(checkMedia);
+
+        Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+            get: function() {
+                return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+            }
+        });
     });
 </script>
